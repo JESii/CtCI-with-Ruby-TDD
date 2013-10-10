f29a2e424a19abd00125bbbc5e835645cb89d841
JESii <jseidel@edpci.com>
2013-10-10 01:16:52 -0700

    Modify exercise 9.4_ subsets...
    Create additional implementation without using ruby's repeated_combination() method
    Refactor the examples to re-use the same 'it' phrases for the two different methods
    Additional documentation about the two approaches.

--- chapter_09/9.4_subsets.rb
+++ chapter_09/9.4_subsets.rb
@@ -4,8 +4,25 @@
 #
 #   Sets are implemented with arrays for this exercise.
 #   Mathematically speaking, the empty set is always a subset.
-#   It's not included in these results, but can easily be added.
-#   (Ruby makes this trivial :=)
+#   It's not included in these results, but can be trivially added.
+#   Note: the methods ignore repeated elements in the initial set:
+#         i.e., {a,a,c} yields only {a},{c},{a,c}
+#   This could be handled by first adding a distinguishing mark to
+#   each element, such as making {a,a} => {a,a'}, and then removing
+#   the distinguishing marks at the end of the algorithm or by 
+#   implementing a different algorthim, such as is done in CtCI.
+#
+#   Implementations
+#   ---------------
+#   (1) return_subsets() method
+#   Ruby makes this a rather easy exercise because of the core
+#   repeated_combination method.
+#
+#   (2) return_subsets_raw() method
+#   So I implemented it without that ruby method, using a recursive approach.
+#   Rather than the 'additive' approach used in CtCI, I instead use what I'll
+#   call a 'reductive' approach in that I start with the full set and repeatedly
+#   remove an element from the candidate set, capturing subsets along the way.
 
 class Subsets
   def initialize set
@@ -16,22 +33,37 @@ class Subsets
     (1..@set.size).each { |n| @set.repeated_combination(n) { |c| @result << c unless c.uniq.size != c.size}}
     @result
   end
+  def return_subsets_raw
+    @result_raw = []
+    ss_raw(@set)
+    @result_raw.uniq
+  end
+  def ss_raw set
+    @result_raw << set
+    return if set.size == 1
+    set.each_index do |i|
+      tmp = set.clone
+      tmp.delete_at i
+      ss_raw(tmp)
+    end
+  end
 end
 
 require 'spec_helper'
 
 describe "Subsets" do
-  it "returns the element for a set of one element" do
-    set = Subsets.new [1]
-    expect(set.return_subsets).to eq [[1]]
-  end
-  it "returns three subsets for a set of two elements" do
-    set = Subsets.new [1,2]
-    set.return_subsets.should =~ [[1],[2],[1,2]]
-  end
-  it "returns all subsets for a set of three elements" do
-    set = Subsets.new [1,2,3]
-    set.return_subsets.should =~ [[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]]
+  %w{return_subsets return_subsets_raw}.each do |method|
+    it "returns the element for a set of one element" do 
+      set = Subsets.new [1]
+      expect(set.__send__(method)).to eq [[1]]
+    end
+    it "returns three subsets for a set of two elements" do
+      set = Subsets.new [1,2]
+      set.__send__(method).should =~ [[1],[2],[1,2]]
+    end
+    it "returns all subsets for a set of three elements" do
+      set = Subsets.new [1,2,3]
+      set.__send__(method).should =~ [[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]]
+    end
   end
 end
-
