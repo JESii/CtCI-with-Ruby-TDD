0241ccedf14acd5cbbdeaf9b9443b8843e295780
Jon Seidel <jseidel@edpci.com>
2014-01-08 09:00:56 -0800

    Adding convenience methods...
    move(from,to) to make a specific move.
    bounce_{left/right} to move the 1 two hops left or right (the basic move).
    move_next() to make next possible move after a bounce, ignoring the 1.

--- chapter_03/3.4_towers_of_hanoi.rb
+++ chapter_03/3.4_towers_of_hanoi.rb
@@ -1,18 +1,45 @@
 # CtCI 3.4
 #   "Implement a method to solve the Towers on Hanoi
 #   Assume 3 towers (pegs)
-### In-progress
+### TODO: In-progress
+# Patterns that solve the game
+# 1. Unstack until the largest disk is on tower #2
+#    - tower #2 is empty and tower #3 has all other disks
+#    - noted as position (*) below
+# 2. Restack disks until on tower #2
+#    - conceptually a 'reverse' of the previous moves
+# - Moves are made in groups of three where 3ach group has three moves
+# - Eeach group moves the 'top' disk left or right two places
+#   and then moves one other disk into an open space
+#   Thus, the #1RRxx or #1LLxx pattern
+# Hmmm: is it as simple as bounce the smaller and make the ONLY move possible?
+# 3-disks
+# #1RR#1R | #3LL#2R | #1RR#1R  => [[],[3],[1,2]] (*)
+# | #3LL#3L | #1R
+# 4-disks
+# #1RR#1R | #3LL#2R | #1RR#1R 
+# #3LL#3L | #1RR#2L | #3LL#2R => [[1,2,4],[],[3]] (1)
+# #1RR#1R | #3LL#2R | #1RR#1R => [[],[4],[1,2,3]] (*)
+# #3LL#3L | #1RR#2L | #1LL#3L => [[1,2],[3,4],[]]
+# #1RR#1R | #3L
+# 5-disks
+# #1RR#1R | #3LL#2R | #1RR#1R 
+# #3LL#3L | #1RR#2L | #3LL#2R => [[1,2,4,5],[],[3]] (2)
+# #1RR#1R | #3LL#2R | #1RR#1R => [[5],[4],[1,2,3]] 
+# #3LL#3L | #1RR#2L | #3LL#3L => [[1,2,5],[3,4],[]]
+# #1RR#1R | #3LL#2R | #1RR#2L => [[3,5],[4],[1,2]]
+# #1LL#3L | #1RR#2L | #1LL#2R => [[1,2,3,5],[],[4]] (1)
+# ... 9 more groups of 3      => [[],[5],[1.2.3.4]] (*)
 
 puts "==================Towers of Hanoi===================="
 
 require 'spec_helper'
 
-#TODO: Think about using a 'Towers' class here
-# =>Methods would be something like:
-#     move_{right/left}       - return false if invalid move
+#TODO: Add methods to the Towers class?
 #     can_move_{right/left}?  - see if the move is valid
+#     move                    - make the next available move
 #     is_done?                - have we completed the tower move?
-# =>  ???more???
+#     ???more???
 
 class Towers
   def initialize(height)
@@ -30,11 +57,55 @@ class Towers
   end
 
   def move_right(tower)
-     @towers[tower].unshift @towers[tower-1].shift
+    @towers[tower].unshift @towers[tower-1].shift
   end
 
   def move_left(tower)
     @towers[tower-2].unshift @towers[tower-1].shift
+  end
+
+  def move_next
+    big = self.biggest
+    small = self.smallest
+    self.move(small,big)
+  end
+
+  def biggest
+    biggest = index = 0
+    @towers.each_with_index do |e,i|
+      next if e[0] == 1
+      if e[0].nil?
+        index = i
+        break
+      end
+      if e[0] > biggest
+        biggest = e[0]
+        index = i
+      end
+    end
+    index
+  end
+  def smallest
+    smallest = 99999
+    index = 0
+    @towers.each_with_index do |e,i|
+      next if e[0] == 1
+      next if e[0].nil?
+      if e[0] < smallest[0]
+        smallest = e
+        index = i
+      end
+    end
+    index
+  end
+  def move(from,to)
+    @towers[to].unshift @towers[from].shift
+  end
+  def bounce_right
+    move(0,2)
+  end
+  def bounce_left
+    move(2,0)
   end
 end
 
@@ -68,7 +139,7 @@ describe "TowersOfHanoi" do
       expect(towers.display_raw).to eql [[2,3],[1],[]]
     end
 
-    it "can move an item to the right" do
+    it "can move an item to the left" do
       towers = Towers.new(3)
       towers.move_right(1)
       towers.move_right(2)
@@ -76,6 +147,26 @@ describe "TowersOfHanoi" do
       expect(towers.display_raw).to eql [[3],[2],[1]]
       towers.move_left(3)
       expect(towers.display_raw).to eql [[3],[1,2],[]]
+    end
+
+    it "makes the next available move" do
+      towers = Towers.new(3)
+      towers.move_right(1)
+      towers.move_right(2)
+      towers.move_next()
+      expect(towers.display_raw).to eql [[3],[2],[1]]
+    end
+    it "can 'bounce' the 1 from left to right" do
+      towers = Towers.new(3)
+      towers.bounce_right
+      expect(towers.display_raw).to eql [[2,3],[],[1]]
+    end
+    it "can 'bounce' the 1 from right to left" do
+      towers = Towers.new(3)
+      towers.bounce_right
+      towers.move_next
+      towers.bounce_left
+      expect(towers.display_raw).to eql [[1,3],[2],[]]
     end
   end
 end
