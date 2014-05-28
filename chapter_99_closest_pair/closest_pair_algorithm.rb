91f7cb8e1b4d9adf572cde7843d97f6c858672a0
Jon Seidel <jseidel@edpci.com>
2014-05-27 18:31:07 -0700

    ClosestPair updates...
    Nodes now created and stored in array; size is properly returned.

--- chapter_99_closest_pair/closest_pair_algorithm.rb
+++ chapter_99_closest_pair/closest_pair_algorithm.rb
@@ -5,6 +5,13 @@ class ClosestPair
   attr_accessor :name
   def initialize(name=nil)
     @name = name
+    @nodes = []
+  end
+  def add_node(name,xpos,ypos)
+    @nodes << Node.new(name, :xpos => xpos, :ypos => ypos)
+  end
+  def size
+    @nodes.size 
   end
 end
 
@@ -22,5 +29,16 @@ describe ClosestPair do
     cpair = ClosestPair.new("header")
     expect(cpair.name).to eql "header"
   end
+  it "accepts a list of nodes for processing" do
+    cpair = ClosestPair.new("two nodes")
+    cpair.add_node("one",5,0.5)
+    expect(cpair.size).to eql 1
+  end
+  it "accepts two nodes" do
+    cpair = ClosestPair.new("two nodes")
+    cpair.add_node("one", 5,0.5)
+    cpair.add_node("two",3,1.2)
+    expect(cpair.size).to eql 2
+  end
 end
-# require_relative '../chapter_02/2.x_linked_list.rb'
+require_relative '../chapter_02/2.x_linked_list.rb'
