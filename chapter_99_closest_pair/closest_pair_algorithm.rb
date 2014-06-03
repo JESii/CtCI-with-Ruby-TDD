f3ca481b81d34315fe92abd56d58255ebf9efd05
Jon Seidel <jseidel@edpci.com>
2014-06-03 04:32:00 -0700

    Add distance calculation to ClosestPair algorithm, with tests

--- chapter_99_closest_pair/closest_pair_algorithm.rb
+++ chapter_99_closest_pair/closest_pair_algorithm.rb
@@ -10,7 +10,7 @@ class ClosestPair
     @nodes = []
   end
   def add_node(name,xpos,ypos)
-    @nodes << Node.new(name, :xpos => xpos, :ypos => ypos)
+    @nodes << Node.new(name, nil, :xpos => xpos, :ypos => ypos)
   end
   def size
     @nodes.size 
@@ -19,7 +19,7 @@ class ClosestPair
     @nodes[item-1]
   end
   def distance(node1,node2)
-   5 
+   Math::sqrt( (node1.xpos - node2.xpos)**2 + (node1.ypos - node2.ypos)**2 )
   end
 end
 
@@ -60,7 +60,14 @@ describe ClosestPair do
       cpair = ClosestPair.new("calculate distance")
       cpair.add_node("one",0,0)
       cpair.add_node("two",3,4)
-      expect(cpair.distance(cpair[1],cpair[2])).to eql 5
+      expect(cpair.distance(cpair[1],cpair[2])).to eql 5.0
+    end
+    it "for another set of two nodes" do
+      cpair = ClosestPair.new("calculate simple distance")
+      cpair.add_node("one",0,0)
+      cpair.add_node("two",1,1)
+      expect(cpair.distance(cpair[1],cpair[2])).to eql(Math::sqrt 2)
     end
   end
+  
 end
