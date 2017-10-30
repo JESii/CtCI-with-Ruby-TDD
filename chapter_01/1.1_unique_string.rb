2532eed99066eabb511ec0c25b19c0e5b74973b4
laylaxh <laylaxh@gmail.com>
2017-10-29 20:20:09 -0700

    Initialize rspec and fix chapter 1 tests so they pass

--- chapter_01/1.1_unique_string.rb
+++ chapter_01/1.1_unique_string.rb
@@ -6,26 +6,25 @@ require'spec_helper'
 
 def hasDups?(s)
   return false if s.size <= 1
-  return s.split('').uniq.size != s.split('').size
-  true
+  s.split('').uniq.size != s.split('').size
 end
 
 describe "hasDups?" do
   it "returns false for an empty or one-element string" do
-    expect(hasDups?("")).to be_false
-    expect(hasDups?("a")).to be_false
+    expect(hasDups?("")).to be false
+    expect(hasDups?("a")).to be false
   end
   it "returns false for a string with no dups" do
-    expect(hasDups?("12")).to be_false
-    expect(hasDups?("123")).to be_false
+    expect(hasDups?("12")).to be false
+    expect(hasDups?("123")).to be false
   end
   it "returns true for a string with dups" do
-    expect(hasDups?("aa")).to be_true
-    expect(hasDups?("abacdea")).to be_true
+    expect(hasDups?("aa")).to be true
+    expect(hasDups?("abacdea")).to be true
   end
   it "properly handles embedded newlines" do
-    expect(hasDups?("abc\n456")).to be_false
-    expect(hasDups?("abc\n456c")).to be_true
-    expect(hasDups?("abc\n456\n")).to be_true
+    expect(hasDups?("abc\n456")).to be false
+    expect(hasDups?("abc\n456c")).to be true
+    expect(hasDups?("abc\n456\n")).to be true
   end
 end
